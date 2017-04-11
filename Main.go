48bf7986515021a2488b5fbd5f805174410dd712
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-04-11 18:27:01 +0200

    Refactoring main

--- Main.go
+++ Main.go
@@ -13,19 +13,21 @@ func main() {
 
 	router := server.NewServerRouter()
 	IP:=config.GetMyIP("lo")
+	serverPort:="8888"
+	clientPort:="8080"
 
 	// server starts listening
 	go func() {
-		log.Fatal(http.ListenAndServe(":8888", router))
+		log.Fatal(http.ListenAndServe(":"+serverPort, router))
 	}()
 
 	// client starts sending
 	go func() {
 		var quit = make(chan int)
-		client.StartSendingMessages(2,IP,"8888","hello!",quit)
+		client.StartSendingMessages(2,IP,serverPort,"hello!",quit)
 		time.AfterFunc(9 * time.Second, func(){close(quit)})
 	}()
 
 	// client starts listening
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+clientPort, router))
 }
